8a05be22de09e5a6a734b620e1c7bc1969237935
Takashi Mori <kanetugu2015@gmail.com>
2022-10-07 11:17:37 +0900

    update latest

---
+++ load-test/test-impl/exment/controller/setup/nop-setup.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ -z ${TEST_TARGET} ]
+then
+    source env/env.bash
+fi
+
+source ${TEST_LOGGER}
+
+#tlog "SETUP TEST..."
---
+++ load-test/test-impl/exment/runner/done/nop-done.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ -z ${TEST_TARGET} ]
+then
+    source env/env.bash
+fi
+
+source ${TEST_LOGGER}
+if [ $# -ne 1 ]
+then
+    terror "Usage: $0 <id>"
+    exit 1
+fi
+ID=${1}
+
+#tlog "ID=${ID}:DONE TEST..."
